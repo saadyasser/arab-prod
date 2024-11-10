615424209376680dcaa7c0c19267eeb34f15780d
rajaa <raja2abdallah98@gmail.com>
2024-11-10 15:02:22 +0200

    update icons as svgs

--- components/ParentLayout/ParentLayout.tsx
+++ components/ParentLayout/ParentLayout.tsx
@@ -2,30 +2,39 @@ import React from "react";
 import TextCard from "../TextCard/TextCard";
 import ImageComponent from "../ImageComponent/ImageComponent";
 import "./ParentLayout.css";
+import { Button } from "@chakra-ui/react";
+import ExamsSectionIcon from "../Svgs/ExamsSectionIcon/ExamsSectionIcon";
+import TestAbilitiesIcon from "../Svgs/TestAbilitiesIcon/TestAbilitiesIcon";
+import MakeChoiceIcon from "../Svgs/MakeChoiceIcon/MakeChoiceIcon";
+import KnowRankingIcon from "../Svgs/KnowRankingIcon/KnowRankingIcon";
 
 const ParentLayout: React.FC = () => {
   return (
     <div className="layout-section">
+      <ImageComponent />
       <div className="card-section">
         <TextCard
-          icon="/images/notes.png"
+          icon={TestAbilitiesIcon}
           title="اختبر قدراتك"
           description="تمكنك الأكاديمية العربية للبرمجة من اختبار مهاراتك البرمجية عبر الإنترنت بسهولة ومرونة. من خلال خدمة الامتحان البرمجي الإلكتروني"
         />
         <TextCard
-          icon="/images/choice.png"
+          icon={MakeChoiceIcon}
           title="قم بالاختيار"
           description="يمكنك اختيار اللغة البرمجية التي تود اختبار معرفتك بها، سواء كانت Python، Java، JavaScript أو أي لغة أخرى من اللغات المتاحة"
         />
         <TextCard
-          icon="/images/certificant.png"
+          icon={KnowRankingIcon}
           title="اعرف ترتيبك"
           description="تيح لك لوحة الشرف معرفة ترتيبك بين الطلاب الذين اجتازوا الامتحان، مما يمنحك فرصة مميزة للتميز والإشادة بإنجازك أمام المجتمع الأكاديمي وزملائك"
         />
-        <button className="explore-button">
-        <img src="/images/check.png" alt="Explore Icon" className="button-icon" /> قسم الامتحانات</button>
+        <div className="btn-wrapper">
+              <Button className="explore-button">
+                <ExamsSectionIcon />
+                قسم الامتحانات
+              </Button>
+            </div>
       </div>
-      <ImageComponent />
     </div>
   );
 };
--- components/TextCard/TextCard.tsx
+++ components/TextCard/TextCard.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import "./TextCard.css";
 
 interface CardProps {
-  icon: string;
+  icon: React.FC<React.SVGProps<SVGSVGElement>>;
   title: string;
   description: string;
 }
 
-const TextCard: React.FC<CardProps> = ({ icon, title, description }) => {
+const TextCard: React.FC<CardProps> = ({ icon:  Icon, title, description }) => {
   return (
     <div className="card">
       <div className="card-header">
-        <img src={icon} alt="Icon" className="card-icon" />
+        <Icon className="card-icon" width={24} height={24} />
         <h3 className="card-title">{title}</h3>
       </div>
       <p className="card-description">{description}</p>
