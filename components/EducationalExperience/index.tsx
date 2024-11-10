0f00f53023c7a7b828ab900f9ba4079c079b2eda
MohammedAlzayyan <mohammedznn1999@gmail.com>
2024-11-10 11:31:54 +0200

    add a left column(title, desc. and button) without style

--- components/EducationalExperience/index.tsx
+++ components/EducationalExperience/index.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 
-import "./EducationalExperience.css";
-
+import EducationIcon from "../Svgs/Education/EducationIcon";
+import { Button } from "../ui/button";
 import ExperienceImage from "@/public/images/ExperienceImage.svg";
+
+import "./EducationalExperience.css";
 
 const EducationalExperience = () => {
   return (
@@ -18,7 +20,34 @@ const EducationalExperience = () => {
           </div>
         </div>
         <div className="column">
-          <div className="left-column">left column</div>
+          <div className="left-column">
+            <div className="experience-card">
+              <h5 className="title">
+                التجربة التعليمية في الأكاديمية العربية للبرمجة
+              </h5>
+              <p className="explanation">
+                الأكاديمية العربية للبرمجة تقدم تجربة تعليمية مميزة وفريدة تركز
+                على إنتاج فيديوهات تعليمية بعناصر تفاعلية وشاملة تناسب جميع
+                الفئات العمرية والمستويات. نسعى لتمكين كل فرد من تعلم البرمجة
+                بطريقة مبسطة وممتعة، مع مراعاة احتياجات المتعلمين وتقديم محتوى
+                يلهمهم للتفوق والإبداع. سواء كنت مبتدئًا أو محترفًا، ستجد لدينا
+                ما يلهمك ويطور مهاراتك في عالم البرمجة، مع دعم مستمر وموارد غنية
+                تواكب أحدث التقنيات والأساليب التعليمية.
+              </p>
+              <div className="btn-wrapper">
+                <Button className="educationButton">
+                  <EducationIcon />
+                  المسارات التعليمية
+                </Button>
+              </div>
+            </div>
+            <div className="btn-wrapper">
+              <Button className="educationButton">
+                <EducationIcon />
+                المسارات التعليمية
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </section>
