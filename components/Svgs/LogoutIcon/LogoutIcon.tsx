8fdc4e333477de5ea28d890464c56f855c538452
MohammedAlzayyan <mohammedznn1999@gmail.com>
2024-11-14 18:28:45 +0200

    add general stuff - globals style, flexbox component and svgs

---
+++ components/Svgs/LogoutIcon/LogoutIcon.tsx
@@ -0,0 +1,57 @@
+export const LogoutIcon = (props: any) => {
+  return (
+    <svg
+      width="49"
+      height="43"
+      viewBox="0 0 49 43"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g filter="url(#filter0_d_596_821)">
+        <path
+          d="M5.16298 15.8786L18.497 1.29767C19.6875 -0.00419617 21.7511 0.907112 21.7511 2.77312L21.7511 11.1051L32.5453 11.1051C33.6009 11.1051 34.4501 12.0337 34.4501 13.1881V21.52C34.4501 22.6743 33.6009 23.603 32.5453 23.603H21.7511V31.9349C21.7511 33.7923 19.6954 34.7123 18.497 33.4104L5.16298 18.8295C4.42484 18.0136 4.42484 16.6944 5.16298 15.8786ZM29.3705 1.73163V5.20327C29.3705 5.77609 29.7991 6.24477 30.323 6.24477L36.9899 6.24477C38.3948 6.24477 39.5298 7.48588 39.5298 9.02208V25.686C39.5298 27.2222 38.3948 28.4633 36.9899 28.4633L30.323 28.4633C29.7991 28.4633 29.3705 28.932 29.3705 29.5048V32.9764C29.3705 33.5493 29.7991 34.0179 30.323 34.0179L36.9899 34.0179C41.1965 34.0179 44.6094 30.2859 44.6094 25.686V9.02208C44.6094 4.42215 41.1965 0.690132 36.9899 0.690132L30.323 0.690132C29.7991 0.690132 29.3705 1.15881 29.3705 1.73163Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_596_821"
+          x="0.609375"
+          y="0.686432"
+          width="48"
+          height="41.3333"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_596_821"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_596_821"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+export default LogoutIcon;
