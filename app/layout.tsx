87571ff4f281f8ca3ac9c119160626a75e82f88a
MohammedAlzayyan <mohammedznn1999@gmail.com>
2024-12-20 15:23:04 +0200

    put navbar and footer in layout, change type of input in search bar from text to search

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
-'use client';
+"use client";
 
-import type { Metadata } from "next";
 import { Provider } from "@/components/ui/provider";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
 import { Tajawal } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/NavBar/NavBar";
+import Footer from "@/sections/Footer/Footer";
+import Banner from "@/components/Banner";
 
 const tajawal = Tajawal({
   subsets: ["arabic"],
@@ -16,16 +18,22 @@ const tajawal = Tajawal({
 
 const queryClient = new QueryClient();
 
-
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="ar" dir="rtl">
       <body className={tajawal.className}>
-      <QueryClientProvider client={queryClient}>
-        <Provider>
-          {children}
+        <QueryClientProvider client={queryClient}>
+          <Provider>
+            <Banner
+              title="خصومات بنسبة 20% على الكورسات"
+              endAt="2022-10-22T12:26:49.668Z"
+              startAt="2022-12-28T12:26:49.668Z"
+            />
+            <NavBar />
+            <main>{children}</main>
+            <Footer />
           </Provider>
-      </QueryClientProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,14 @@ import ParentLayout from "@/components/ParentLayout/ParentLayout";
 import EducationalExperience from "@/components/EducationalExperience/EducationalExperience";
 import InteractiveToolsSection from "@/components/InteractiveToolsSection/InteractiveToolsSection";
 import UsersReviews from "@/sections/UsersReviews/UsersReviews";
-import Carousel from "@/components/Carousel/Carousel";
 import TrainingCourses from "@/sections/TrainingCourses/TrainingCourses";
 import CardCarousel from "@/components/CardCarousel/CardCarousel";
-import Footer from "@/sections/Footer/Footer";
-import NavBar from "@/components/NavBar/NavBar";
 import HeroSection from "@/sections/HeroSection/HeroSection";
 import Video from "@/components/Video/Video";
 
 export default function Home() {
-  const items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"];
   return (
     <div>
-      <NavBar />
       <HeroSection />
       <Video />
       <CardCarousel />
@@ -24,7 +19,6 @@ export default function Home() {
       <EducationalExperience />
       <InteractiveToolsSection />
       <UsersReviews />
-      <Footer />
     </div>
   );
 }
