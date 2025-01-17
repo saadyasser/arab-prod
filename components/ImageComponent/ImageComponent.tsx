bc4f80d7869d5e56437d37505ead01fc1e77a812
MohammedAlzayyan <mohammedznn1999@gmail.com>
2025-01-17 15:32:48 +0200

    fix some issues in Exam section desgin

--- components/ImageComponent/ImageComponent.tsx
+++ components/ImageComponent/ImageComponent.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "./ImageComponent.css";
+import Image from "next/image";
 
 const ImageComponent: React.FC = () => {
   return (
     <div className="image-section">
-      <img src="/images/human2.jpeg" alt="Image 1" className="image image-1" />
-      <img src="/images/human1.jpeg" alt="Image 2" className="image image-2" />
+      <Image
+        src="/images/human2.jpeg"
+        alt="Image 1"
+        width={100}
+        height={100}
+        className="image image-1"
+      />
+      <Image
+        src="/images/human1.jpeg"
+        alt="Image 1"
+        width={100}
+        height={100}
+        className="image image-2"
+      />
+      {/* <img src="/images/human2.jpeg" alt="Image 1" className="image image-1" />
+      <img src="/images/human1.jpeg" alt="Image 2" className="image image-2" /> */}
     </div>
   );
 };
